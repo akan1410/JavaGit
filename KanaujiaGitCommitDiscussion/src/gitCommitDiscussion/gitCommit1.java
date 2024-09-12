6493cc7dfabe35c26c6c26b93bde14e30749cfa1
akan1410 <ayushkanaujiaa99@gmail.com>
2024-09-12 10:09:17 -0500

    changed the print statement

--- KanaujiaGitCommitDiscussion/src/gitCommitDiscussion/gitCommit1.java
+++ KanaujiaGitCommitDiscussion/src/gitCommitDiscussion/gitCommit1.java
@@ -11,6 +11,6 @@ package gitCommitDiscussion;
  */
 public class gitCommit1 {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello World. I am coming for you!");
     }
 }
